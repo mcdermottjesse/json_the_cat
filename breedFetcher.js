35c066c8679154ca85d8ae75ce9e152d3cca4844
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-02-12 20:40:45 +0000

    main breedfecther file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,35 @@
-const request = require('request')
+const request = require('request');
 
-const url =  'https://api.thecatapi.com/v1/breeds/search';
-const breed = process.argv[2]
-const input = `${url}?q=${breed}`
+const url = 'https://api.thecatapi.com/v1/breeds/search';
+//const breed = process.argv[2]
+
+//const input = `${url}?q=${breed}`
 
 
 
-request(input, (error, response, body) => {
-  console.log(body)
- if (error) {
-   console.log("url does not exist")
- } else if(response.statusCode === 404) {
-   console.log("path does not exist")
+const fetchBreedDescription = function(breed, callback) {
+  const input = `${url}?q=${breed}`;
+  request(input, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    } else if (response.statusCode === 404) {//The response.statusCode indicates the HTTP response code
+      return callback("path does not exist", null);
 
- }
+    }
 
- const request = JSON.parse(body);
- console.log(request)
- console.log(response.statusCode)
- if (request[0] === undefined) {
-console.log(`The requested breed ${breed} is not found`)
- } else {
-   console.log(request[0].description)
- }
-}); 
+    const request = JSON.parse(body);
+    //console.log(request)
+    console.log(response.statusCode);
+    if (request[0] === undefined) {
+      return callback(null, `The requested breed ${breed} is not found`);
+    } else {
+      return callback(null, request[0].description);
+
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
 
 
